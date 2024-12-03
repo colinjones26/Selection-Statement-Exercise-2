7c402f936696fec1ef1726119cb6d7b1dd699c43
colinjones26 <colinanthonyjones1996@gmail.com>
2024-12-03 15:05:34 -0500

    intial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            //Ask user for favorite school subject
+            Console.WriteLine("What is your favorite school subject?");
+
+            //Storing user answer
+            string Subject = Console.ReadLine();
+
+            //Switch statements
+            switch (Subject)
+
+            {
+                case "Math":
+                    Console.WriteLine("You hate Math! It's too difficult.");
+                    break;
+                case "Science":
+                    Console.WriteLine("You love science! You can create experiments!");
+                    break;
+                case "History":
+                    Console.WriteLine("History tells you about the people of the past!");
+                    break;
+                case "Gym Class":
+                    Console.WriteLine("You and everyone else loves gym class!");
+                    break;
+                case "Art":
+                    Console.WriteLine("You love to draw and paint!");
+                    break;
+                default:
+                    Console.WriteLine("I do not know much of this subject");
+                    break;
+            }
+         }
     }
 }
